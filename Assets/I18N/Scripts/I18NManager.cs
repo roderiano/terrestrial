9ed6d157f2e329a5f0a2cb5b089de8b73f876c3e
Gabriel Silveira <gabrielsilveirapro@gmail.com>
2020-07-15 23:33:06 -0300

    Optimized the I18NManager without attach I18NEntity in UI components

--- Assets/I18N/Scripts/I18NManager.cs
+++ Assets/I18N/Scripts/I18NManager.cs
@@ -1,30 +1,75 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+ using System.Text.RegularExpressions;
+
+public class I18NEntity
+{
+    public Object obj;
+    public string token;
+
+    public I18NEntity(Object obj, string token)
+    {
+        this.obj = obj;
+        this.token = token;
+    }
+}
 
 public class I18NManager : MonoBehaviour
 {
-    private I18NEntity[] entities = null;
-
+    [SerializeField]
+    private List<I18NEntity> entities = new List<I18NEntity>();
+    
     void Start()
     {
         FindAllEntities();
         RefreshSceneTranslation(Language.Portuguese);
     }
 
+    // Find all I18NEntity in scene when start
     void FindAllEntities() 
     {
-        entities = (I18NEntity[])FindObjectsOfType(typeof(I18NEntity));
+        List<Object> objects = new List<Object>();
+        
+        // Get all object of type Text and add to list of objects
+        Object[] arrayObjects = FindObjectsOfType(typeof(Text));
+        foreach(Object obj in arrayObjects)
+            objects.Add(obj);
+
+
+        // Verify if obj is a I18Entity and add to entity list
+        string token = "";
+        foreach(Object obj in objects) 
+        {
+            switch(obj) 
+            {
+                case Text text:
+                    token = GetToken(((Text)obj).text);
+                    entities.Add(new I18NEntity(obj, token));
+                break;
+            }
+        }   
     }
 
     void RefreshSceneTranslation(Language language)
     {
-        string translation = "";
         foreach(I18NEntity entity in entities) 
         {
-            translation = GetTranslation(entity.GetToken(), language);
-            entity.SetTranslation(translation);
-        }   
+            string translation = GetTranslation(entity.token, language);
+            if(translation != null)
+            {
+                switch(entity.obj) 
+                {
+                    case Text textComponent:
+                        textComponent.text = translation;
+                    break;
+                }
+            }
+        }  
     }
 
+    
+    // Find translation in CSV
     string GetTranslation(string token, Language language) 
     {
         string path = "Assets/I18N/Data/TranslationData.csv";
@@ -39,5 +84,13 @@ public class I18NManager : MonoBehaviour
         }
 
         return null;        
+    }
+
+    // Find token by regex
+    string GetToken(string text) 
+    {
+        string token = Regex.Match(text, "(token.)(\\S*)").ToString().Replace("token.", "").Trim();
+        token = token != "" ? token : null;
+        return token;
     }
 }
