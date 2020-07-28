0355fa078f053fba92d6d39bba9d865086106248
Gabriel Silveira <gabrielsilveirapro@gmail.com>
2020-07-28 19:08:35 -0300

    Added enemy thrower base script

---
+++ Assets/Enemies/Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class Enemy : MonoBehaviour {
+ 
+ private float health = 100;
+ 
+ public void TakeDamage(float damage)
+ {
+    health -= damage;
+ }
+ 
+}
---
+++ Assets/Enemies/Thrower/ThrowerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowerController : Enemy
+{
+    [SerializeField]private Transform reloadPoint;
+    [SerializeField]private Transform shotRoot;
+
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = target.position - shotRoot.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            shotRoot.eulerAngles = rotation.eulerAngles - new Vector3(0f, 0f, 180f);
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(reloadPoint.position, 1f);  
+        Gizmos.DrawWireSphere(shotRoot.Find("ShotPoint").position, 1f);  
+    }
+}
