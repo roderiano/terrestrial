5ed8d399c0b2c6501bc1c484b251b908c2a6bc1a
Erick Teixeira <erickhenriquemota@gmail.com>
2020-08-02 16:51:40 -0300

    Added basic parallax effect

---
+++ Assets/Maps/Scripts/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    
+    private float startPos;
+    public float parallaxEffect;
+    private Camera mainCam;
+
+    void Start()
+    {
+        startPos = transform.position.x;
+        mainCam = Camera.main;
+    }
+
+    void FixedUpdate()
+    {
+        float dist = (mainCam.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+    }
+}
